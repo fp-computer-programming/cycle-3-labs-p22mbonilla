fec3d4ab8c6d68dc43b70fc75bf2d4274a230ba7
p22mbonilla <p22mbonilla@fairfieldprep.org>
2021-10-04 09:57:03 -0400

    5-1

---
+++ lab_5-1.py
@@ -0,0 +1,26 @@
+# Author MB 10/04/2021
+
+"""
+int(a)
+name error
+"""
+
+"""
+int('a')
+value error
+"""
+
+"""
+'a'+2
+type error
+"""
+
+"""
+import date
+modulenotfound error
+"""
+
+"""
+print("I'm a happy camper!)
+syntax error
+"""
